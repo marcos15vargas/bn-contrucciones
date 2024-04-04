901e9784654f1229c37b043d5da3196bb1f2422e
Marcos Vargas <101339514+marcos15vargas@users.noreply.github.com>
2024-04-04 19:12:59 -0300

    creacion e implementacion de la seccion de proyects

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import { Banner } from "./components/Banner"
-import { Features } from "./components/Features"
-import { Navbar } from "./components/Navbar"
-import { Slider } from "./components/Slider"
+import { Banner } from "./components/Banner";
+import { Features } from "./components/Features";
+import { Navbar } from "./components/Navbar";
+import { Proyects } from "./components/Proyects";
+import { Slider } from "./components/Slider";
 
 function App() {
-
   return (
     <>
       <Navbar />
       <Banner />
       <Features />
-      <Slider/>
+      <Proyects />
+      <Slider />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Proyects.jsx
@@ -0,0 +1,47 @@
+import img1 from "../assets/proyects/1.webp";
+import img2 from "../assets/proyects/2.webp";
+import img3 from "../assets/proyects/3.webp";
+import img4 from "../assets/proyects/4.webp";
+
+export const Proyects = () => {
+  return (
+    <section className="flex flex-col md:flex-row max-w-7xl items-center m-auto my-12">
+      <div className="w-full md:w-1/2 p-8 self-center">
+        <h3 className="font-bold text-3xl lg:text-4xl underline decoration-azul">
+          Nuestros Trabajos
+        </h3>
+        <p className="text-justify my-10">
+          Somos una empresa constructora de sólida trayectoria y experiencia,
+          con un equipo profesional plenamente comprometido con el cumplimiento
+          y capaz de brindar múltiples soluciones en forma rápida, creativa y
+          eficaz.
+          <br />
+          <br />
+          Nuestro esfuerzo se refleja en obras, tanto privadas como públicas,
+          que mejoran la calidad de vida de las personas y agregan valor a
+          nuestros clientes.
+        </p>
+        <a className="font-bold text-xl text-azul underline" href="#">
+          Ver mas
+        </a>
+      </div>
+      <div className="w-full md:w-1/2">
+        <div className="grid grid-cols-4 grid-rows-4 gap-4 p-8 m-auto">
+          <div className="col-span-2 row-span-2 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+            {/* 860x519 */}
+            <img className="cover" src={img2} alt="" />
+          </div>
+          <div className="col-span-2 row-span-2 col-start-3 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+            <img className="object-cover" src={img3} alt="" />
+          </div>
+          <div className="col-span-2 row-span-2 row-start-3 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+            <img src={img4} alt="" />
+          </div>
+          <div className="col-span-2 row-span-2 col-start-3 row-start-3 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+            <img src={img1} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
