df11ee8a36fc38f51c4ada9b1bfcef32606a6526
Marcos Vargas <101339514+marcos15vargas@users.noreply.github.com>
2024-06-27 23:48:15 -0300

    update de la ruta del componente Stats en el Home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,14 +2,14 @@ import Banner from "./components/Banner";
 import Features from "./components/Features";
 import Proyects from "./components/Proyects";
 import Gallery from "./components/Gallery";
-import Stats from "./components/Stats";
+import Stats from "../../components/Stats";
 import Testimonial from "./components/Testimonial";
 
 
 
 const Home = () => {
   return (
-    <main>
+    <main className="text-black">
       <Banner />
       <Stats />
       <Features />
