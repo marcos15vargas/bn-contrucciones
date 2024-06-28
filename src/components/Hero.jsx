8b4c88a523b01d828ff9519c678dff485e3f37d5
Marcos Vargas <101339514+marcos15vargas@users.noreply.github.com>
2024-06-27 22:21:39 -0300

    cambio al componente de hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = ( {title, image} ) => {
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-neutral-content text-center">
         <div className="max-w-md">
-          <h1 className="mb-5 text-2xl lg:text-5xl  font-bold">{title}</h1>
+          <h1 className="mb-5 text-white text-2xl lg:text-5xl font-bold">{title}</h1>
         </div>
       </div>
     </div>
