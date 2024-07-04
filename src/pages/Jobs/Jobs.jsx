155a00f5b5070fc10acea9e1bf439a20ac904cc9
marcos15vargas <marcos15vargas15@gmail.com>
2024-07-04 16:48:26 -0300

    cambios pequeños

--- src/pages/Jobs/Jobs.jsx
+++ src/pages/Jobs/Jobs.jsx
@@ -1,8 +1,7 @@
 import Hero from "../../components/Hero";
 import Modals from "./components/Modals";
 
-const image =
-  "https://cdn.pixabay.com/photo/2024/04/05/05/17/business-8676529_640.jpg";
+const image = "https://cdn.pixabay.com/photo/2024/04/05/05/17/business-8676529_640.jpg";
 
 const Jobs = () => {
   return (
--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,7 +1,11 @@
+import Hero from "../../components/Hero"
 
+const image = "https://cdn.pixabay.com/photo/2024/04/05/05/17/business-8676529_640.jpg"
 const Services = () => {
   return (
-    <div>Services</div>
+    <>
+      <Hero title="Servicios" image={image} />
+    </>
   )
 }
 
