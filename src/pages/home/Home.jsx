04a94ce23107625dfccfc8b25495e52081382873
marcos15vargas <marcos15vargas15@gmail.com>
2024-07-06 19:51:19 -0300

    creacion de Banner aleternativo para el home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,18 @@
-import Banner from "./components/Banner";
+// import Banner from "./components/Banner";
 import Features from "./components/Features";
 import Proyects from "./components/Proyects";
 import Gallery from "./components/Gallery";
 import Stats from "../../components/Stats";
 import Testimonial from "./components/Testimonial";
+import Banner2 from "./components/Banner2";
 
 
 
 const Home = () => {
   return (
     <main className="text-black">
-      <Banner />
+      {/* <Banner /> */}
+      <Banner2 />
       <Stats />
       <Features />
       <Proyects />
