a2b9880ffabbe52769e26a7a93ec178ea35cd283
marcos15vargas <marcos15vargas15@gmail.com>
2024-07-16 18:42:09 -0300

    arreglo en secicon neforeAfter de pagina Trabajos

--- src/pages/Jobs/components/BeforeAfter.jsx
+++ src/pages/Jobs/components/BeforeAfter.jsx
@@ -4,12 +4,12 @@ import imageAfter from "../../../assets/beforeAfter/despues.webp";
 const BeforeAfter = () => {
   return (
     <section className="w-full">
-      <div className="mx-auto max-w-6xl border-2 border-red-600">
+      <div className="mx-auto max-w-6xl py-8">
         <h2 className="text-4xl py-2 font-bold text-black underline decoration-azul">
           Antes y Despues
         </h2>
-        <div className="bebore-after p-4 flex">
-          <div className="diff aspect-[3/2] max-w-xl h-[500px]">
+        <div className="bebore-after p-4 flex justify-center">
+          <div className="diff aspect-[3/2] max-w-xl h-[600px]">
             <div className="diff-item-1">
               <img alt="daisy" src={imageAfter} />
             </div>
